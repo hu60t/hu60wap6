7eb77177426a240ebb6cc5bf27fdafec72ec21df
hu60.cn@gmail.com <hu60.cn@gmail.com@7c7e4541-cd8d-e136-b550-92fd32b3d32a>
2012-06-15 14:05:48 +0000

    hu60t重大更新，hu60wap6重新复制。删除原来的。
    git-svn-id: http://hu60t.googlecode.com/svn/hu60wap6@34 7c7e4541-cd8d-e136-b550-92fd32b3d32a

--- trunk/class/smarty/plugins/compiler.enddiv.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* </div> */
-function smarty_compiler_enddiv($p,$smarty)
-{
-global $PAGE;
-return $PAGE['bid']=='wml' ? (isset($p['wml']) ? "<?php echo $p[wml];?>" : "</p>") : "</div>";
-}
--- trunk/class/smarty/plugins/compiler.endspan.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* </span> */
-function smarty_compiler_endspan($p,$smarty)
-{
-global $PAGE;
-return $PAGE['bid']=='wml' ? (isset($p['wml']) ? "<?php echo $p[wml];?>" : "") : "</span>";
-}
