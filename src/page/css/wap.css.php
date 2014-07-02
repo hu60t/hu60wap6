132b62cf9c0facca329a865d918e8c616903b497
default_git_name <default_git_email>
2014-07-02 07:06:50 +0000

    change css

--- src/page/css/wap.css.php
+++ src/page/css/wap.css.php
@@ -4,6 +4,7 @@ $tpl=$PAGE->start();
 @ini_set('default_charset',NULL);
 header('Content-type: text/css');
 $css=str::word($PAGE->ext[0],true);
+$bid=str::word($PAGE->ext[1],true);
 if($css!='') {
  setCookie(COOKIE_A.'page_css_wap',$css,$_SERVER['REQUEST_TIME']+DEFAULT_LOGIN_TIMEOUT,COOKIE_PATH,COOKIE_DOMAIN);
  $USER->start($tpl);
@@ -18,7 +19,7 @@ if($css!='') {
  }
 }
  if($css=='') $css='default';
-$tpl->display($x='tpl:css.wap_'.$css.'.css');
+$tpl->display($x='tpl:'.$bid.'.wap_'.$css.'.css');
 } catch(exception $ERR) {
 throw $ERR;
  }
