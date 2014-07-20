ba7349acc8670d5037b40288507ad6aad1465808
shenpuvip <shenpuvip@qq.com>
2014-07-21 07:19:53 +0800

    格式化文件

--- src/page/addin/chat.php
+++ src/page/addin/chat.php
@@ -1,43 +1,43 @@
 <?php
-$tpl=$PAGE->start();
-$USER->start();
-$user=$USER;
+$tpl = $PAGE -> start();
+$USER -> start();
+$user = $USER;
 $chat = new chat;
-if($PAGE->ext[0]){
-$roomname = $PAGE->ext[0];
-$tpl->assign('roomname',$roomname);
-$chat->checkroom($roomname);
-if($_POST['go'])
-{
-if(!$user->islogin)
-$err_msg = '你必须要<a href="user.login.'.$PAGE->bid.'">登录</a>才能发言';
-else
-{
-if($_POST['neirong']=='')
-$err_msg = '内容不能为空';
-else
-{
-$ubb = new ubbparser;
-$content = $ubb->parse($_POST['neirong'],true);
-$chat->chatsay($roomname,$user->uid,$user->name,$content,time());
-}
-}
-}
-$ubbs = new ubbdisplay();
-$tpl->assign('err_msg',$err_msg);
-$list = $chat->chatlist($roomname);
-foreach($list[row] as $k=>$m){
-$list[row][$k][content] = $ubbs->display($m[content],truw);
-}
-$tpl->assign('list',$list);
-$tpl->display("tpl:chat");
-}else{
-if($_POST[roomname]){
-$url = 'addin.chat.'.$_POST[roomname].'.'.$PAGE->bid;
-header("location:$url");
-}
-//聊天室列表
-$list = $chat->roomlist();
-$tpl->assign('list',$list);
-$tpl->display("tpl:chat_list");
-}
+if($PAGE -> ext[0]){
+     $roomname = $PAGE -> ext[0];
+     $tpl -> assign('roomname', $roomname);
+     $chat -> checkroom($roomname);
+     if($_POST['go'])
+    {
+         if(!$user -> islogin)
+             $err_msg = '你必须要<a href="user.login.' . $PAGE -> bid . '">登录</a>才能发言';
+         else
+            {
+             if($_POST['neirong'] == '')
+                 $err_msg = '内容不能为空';
+             else
+                {
+                 $ubb = new ubbparser;
+                 $content = $ubb -> parse($_POST['neirong'], true);
+                 $chat -> chatsay($roomname, $user -> uid, $user -> name, $content, time());
+                 }
+             }
+         }
+     $ubbs = new ubbdisplay();
+     $tpl -> assign('err_msg', $err_msg);
+     $list = $chat -> chatlist($roomname);
+     foreach($list[row] as $k => $m){
+         $list[row][$k][content] = $ubbs -> display($m[content], truw);
+         }
+     $tpl -> assign('list', $list);
+     $tpl -> display("tpl:chat");
+     }else{
+     if($_POST[roomname]){
+         $url = 'addin.chat.' . $_POST[roomname] . '.' . $PAGE -> bid;
+         header("location:$url");
+         }
+     // 聊天室列表
+    $list = $chat -> roomlist();
+     $tpl -> assign('list', $list);
+     $tpl -> display("tpl:chat_list");
+     }
--- src/page/msg/index.php
+++ src/page/msg/index.php
@@ -1,49 +1,51 @@
 <?php
-$tpl=$PAGE->start();
-$USER->start();
-$user=$USER;
-if(!$user->islogin){header('location:user.login.'.$PAGE->bid.'');}
+$tpl = $PAGE -> start();
+$USER -> start();
+$user = $USER;
+if(!$user -> islogin){
+     header('location:user.login.' . $PAGE -> bid . '');
+     }
 $msg = new msg();
 $uinfo = new userinfo;
 $ubb = new ubbparser;
 $ubbs = new ubbdisplay();
-if($PAGE->ext[0] == 'outbox'){
-//发件箱
-$list = $msg->read_outbox($user->uid,$PAGE->ext[1]);
-foreach($list[row] as $k=>$m){
-$uinfo->uid($m['touid']);
-$list[row][$k]['toname'] = $uinfo->name;
-$list[row][$k]['content'] = $ubbs->display($m['content'],true);
-}
-$tpl->assign('list',$list);
-$tpl->display('tpl:outbox');
-}elseif($PAGE->ext[0] == 'send'){
-//发送信息
-if($_POST){
-$content = $ubb->parse($_POST['content'],true);
-$send = $msg->send_msg($user->uid,$_POST[touid],$content);
-$tpl->assign('send',$send);
-}
-$tpl->assign('touid',$PAGE->ext[1]);
-$tpl->display('tpl:send');
-}elseif($PAGE->ext[0] == 'view' && $PAGE->ext[1]){
-//查看信息
-$xx = $msg->read_msg($user->uid,$PAGE->ext[1]);
-$uinfo->uid($xx[touid]);
-$xx[toname] = $uinfo->name;
-$uinfo->uid($xx[byuid]);
-$xx[byname] = $uinfo->name;
-$xx['content'] = $ubbs->display($xx['content'],true);
-$tpl->assign('msg',$xx);
-$tpl->display('tpl:view');
-}else{
-//收件箱
-$list = $msg->read_inbox($user->uid,$PAGE->ext[1]);
-foreach($list[row] as $k=>$m){
-$uinfo->uid($m['byuid']);
-$list[row][$k]['byname'] = $uinfo->name;
-$list[row][$k]['content'] = $ubbs->display($m['content'],true);
-}
-$tpl->assign('list',$list);
-$tpl->display('tpl:inbox');
-}
+if($PAGE -> ext[0] == 'outbox'){
+     // 发件箱
+    $list = $msg -> read_outbox($user -> uid, $PAGE -> ext[1]);
+     foreach($list[row] as $k => $m){
+         $uinfo -> uid($m['touid']);
+         $list[row][$k]['toname'] = $uinfo -> name;
+         $list[row][$k]['content'] = $ubbs -> display($m['content'], true);
+         }
+     $tpl -> assign('list', $list);
+     $tpl -> display('tpl:outbox');
+     }elseif($PAGE -> ext[0] == 'send'){
+     // 发送信息
+    if($_POST){
+         $content = $ubb -> parse($_POST['content'], true);
+         $send = $msg -> send_msg($user -> uid, $_POST[touid], $content);
+         $tpl -> assign('send', $send);
+         }
+     $tpl -> assign('touid', $PAGE -> ext[1]);
+     $tpl -> display('tpl:send');
+     }elseif($PAGE -> ext[0] == 'view' && $PAGE -> ext[1]){
+     // 查看信息
+    $xx = $msg -> read_msg($user -> uid, $PAGE -> ext[1]);
+     $uinfo -> uid($xx[touid]);
+     $xx[toname] = $uinfo -> name;
+     $uinfo -> uid($xx[byuid]);
+     $xx[byname] = $uinfo -> name;
+     $xx['content'] = $ubbs -> display($xx['content'], true);
+     $tpl -> assign('msg', $xx);
+     $tpl -> display('tpl:view');
+     }else{
+     // 收件箱
+    $list = $msg -> read_inbox($user -> uid, $PAGE -> ext[1]);
+     foreach($list[row] as $k => $m){
+         $uinfo -> uid($m['byuid']);
+         $list[row][$k]['byname'] = $uinfo -> name;
+         $list[row][$k]['content'] = $ubbs -> display($m['content'], true);
+         }
+     $tpl -> assign('list', $list);
+     $tpl -> display('tpl:inbox');
+     }
