0e2db858b3739da0cabc846b972b85a6dcd928d6
SwimmingTiger <hu60.cn@gmail.com>
2016-02-14 10:36:33 +0800

    删除测试用的0.php；\n将admin/index.php中的短标记改为长标记，并仅允许uid1使用。

--- src/0.php
+++
@@ -1,5 +0,0 @@
-<?php
-include './config.inc.php';
-
-$s = new search();
-var_dump($s->searchTopic('代码工人'));
--- src/page/admin/index.php
+++ src/page/admin/index.php
@@ -1,6 +1,6 @@
-<?
+<?php
 $tpl=$PAGE->start();
 $USER->start($tpl);
-if (!$USER->islogin || $USER->uid != 1 && $USER->uid != 2)
+if (!$USER->islogin || $USER->uid != 1)
     die('403 Forbidden');
 $tpl->display('tpl:index');
