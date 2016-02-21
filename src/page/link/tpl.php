6d3bcb78b9982fa837e05d9df986fa9947bde27e
SwimmingTiger <hu60.cn@gmail.com>
2016-02-21 12:29:05 +0800

    试图解决因先前的cookie导致的无法切换皮肤的问题

--- src/page/link/tpl.php
+++ src/page/link/tpl.php
@@ -3,6 +3,7 @@ $url = trim(code::b64d($_GET['url64']));
 $tpl = str::word($PAGE->ext[0]);
 
 if ($PAGE->isRegTpl($tpl)) {
+	setCookie(COOKIE_A.'tpl', false, -1);
 	$PAGE->setCookie('tpl', $tpl, 3600*24*3650);
 }
 
