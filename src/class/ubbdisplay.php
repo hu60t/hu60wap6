3f5781e44919508bc970a0372abb78467c339532
root <root@vultr.guest>
2016-10-18 10:58:01 +0000

    过滤可能导致版面长草的阿拉伯文

--- src/class/ubbdisplay.php
+++ src/class/ubbdisplay.php
@@ -57,6 +57,7 @@ class ubbDisplay extends XUBBP
         $text = $data['value'];
 
         $text = preg_replace('/[\x{0e00}-\x{0e7f}]{10,}/u', '(为防止版面长草，过长的泰文被过滤)', $text);
+	$text = preg_replace('/[\x{0600}-\x{06ff}]{10,}/u', '(为防止版面长草，过长的阿拉伯文被过滤)', $text);
 
         return code::html($text, '<br/>');
     }
