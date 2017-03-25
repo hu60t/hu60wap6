d672096a828875806e01e8c5c989009fcbfe0f3a
SwimmingTiger <hu60.cn@gmail.com>
2017-03-25 21:31:51 +0800

    添加网页插件专用的数据存储/获取API。

---
+++ src/page/api/webplug-data.php
@@ -0,0 +1,44 @@
+<?php
+jsonpage::start();
+
+try {
+	$USER->start();
+	
+	if (!$USER->islogin) {
+		jsonpage::output(['success'=>false, 'islogin'=>false, 'errmsg'=>'请先登录']);
+	}
+	else {
+		// 获取值
+		if (!isset($_GET['value']) && !isset($_POST['value'])) {
+			if (!isset($_GET['key']) && !isset($_POST['key'])) {
+				jsonpage::output(['success'=>true, 'islogin'=>$USER->islogin, 'data'=>$USER->getinfo('webplugData')]);
+			}
+			else {
+				$key = isset($_GET['key']) ? $_GET['key'] : $_POST['key'];
+				$key = substr(str::word($key), 0, 100);
+				jsonpage::output(['success'=>true, 'islogin'=>$USER->islogin, 'data'=>$USER->getinfo("webplugData.$key")]);
+			}
+		}
+		// 设置值
+		else {
+			$value = isset($_GET['value']) ? $_GET['value'] : $_POST['value'];
+			
+			if (!isset($_GET['key']) && !isset($_POST['key'])) {
+				jsonpage::output(['success'=>false, 'islogin'=>$USER->islogin, 'errmsg'=>'键不能为空']);
+			}
+			else {
+				$key = isset($_GET['key']) ? $_GET['key'] : $_POST['key'];
+				$key = substr(str::word($key), 0, 100);
+				
+				$value = substr($value, 0, 1024);
+				
+				$USER->setinfo("webplugData.$key", $value);
+				
+				jsonpage::output(['success'=>true, 'islogin'=>$USER->islogin]);
+			}
+		}
+	}
+}
+catch (Exception $e) {
+	jsonpage::output(['success'=>false, 'islogin'=>$USER->islogin, 'errmsg'=>$e->getMessage()]);
+}
