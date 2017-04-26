024dac4fd180000f4d838daa9de1633c0d7beeac
SwimmingTiger <hu60.cn@gmail.com>
2017-04-26 03:36:10 +0000

    为了安全，link.url.html会去除链接开头的javascript:

--- src/page/link/url.php
+++ src/page/link/url.php
@@ -3,6 +3,8 @@ $tpl = $PAGE->start();
 $USER->start($tpl);
 
 $url = code::b64d($_GET['url64']);
+$url = preg_replace('/^\s*javascript\s*:/is', '', $url);
+
 $tpl->assign('url', $url);
 
 $tpl->display('tpl:url');
