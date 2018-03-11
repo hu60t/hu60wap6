c403b01c4f32626fd8d9213dcf6b0b1eb55776e6
SwimmingTiger <hu60.cn@gmail.com>
2018-03-12 00:40:16 +0800

    短信验证码提升到6位，使用密码学安全的随机数发生器；支持通过POST请求验证码发送服务。

--- src/class/seccodesms.php
+++ src/class/seccodesms.php
@@ -9,12 +9,34 @@ class secCodeSms implements secCodeInterface
     //发送验证码到手机
     public function sendCode($phone)
     {
-        $code = mt_rand(10000, 99999);
+		if (function_exists('random_int')) {
+			// 密码学安全随机数发生器
+			$code = random_int(100000, 999999);
+		} else {
+			// 非安全随机数发生器，结果可能被预测到
+			$code = mt_rand(100000, 999999);
+		}
 
         $url = str_replace('{@phone}', urlencode($phone), SECCODE_SMS_URL);
         $url = str_replace('{@code}', urlencode($code), $url);
+		
+		// context 选项
+		$opts = [
+			'method'  => 'GET',
+			'timeout' => 15,
+		];
+		
+		if (SECCODE_SMS_METHOD != 'GET') {
+			$data = str_replace('{@phone}', urlencode($phone), SECCODE_SMS_POST_DATA);
+			$data = str_replace('{@code}', urlencode($code), $data);
+			
+			$opts['method'] = SECCODE_SMS_METHOD;
+			$opts['header'] = 'Content-Type: '.SECCODE_SMS_POST_MIME;
+			$opts['content'] = $data;
+		}
 
-        $result = file_get_contents($url);
+		$context = stream_context_create([ 'http' => $opts ]);
+        $result = file_get_contents($url, false, $context);
 
         if (false !== strpos($result, SECCODE_SMS_SUCCESS_FLAG)) {
             return $code;
--- src/config/security.default.php
+++ src/config/security.default.php
@@ -3,6 +3,7 @@
  * 安全性设置
  */
 
+///////////////////// 用户密码加密 /////////////////////
 
 /**
  * 加密用户密码用的Key
@@ -10,6 +11,9 @@
  * 注意：此Key仅可在网站没有任何用户时更改。否则一但更改，所有用户的密码都将失效！
  */
 define('USER_PASS_KEY', 'USER_PASS_KEY');
+
+
+///////////////////// 用户激活短信验证 /////////////////////
 
 /**
  * 是否启用短信验证码
@@ -26,9 +30,30 @@ define('SECCODE_SMS_ENABLE', false);
 define('SECCODE_SMS_URL', 'http://hu60.cn/sms?key=hu60&phone={@phone}&code={@code}');
 
 /**
+ * 短信验证码请求的方式
+ *
+ * 可选值：GET、POST等HTTP方法，区分大小写。
+ */
+define('SECCODE_SMS_METHOD', 'GET');
+
+/**
+ * 短信验证码POST数据，若使用GET方式发送，则无需关心
+ *
+ * 可使用的变量：
+ *    {@phone}    手机号码
+ *    {@code}    欲发送的验证码
+ */
+define('SECCODE_SMS_POST_DATA', 'key=hu60&phone={@phone}&code={@code}');
+
+/**
+ * 短信验证码POST数据的MIME类型，若使用GET方式发送，则无需关心
+ */
+define('SECCODE_SMS_POST_MIME', 'application/x-www-form-urlencoded');
+
+/**
  * 短信验证码发送成功标志
  *
- * 将其设为发送成功时URL会返回的内容
+ * 将其设为发送成功时URL会返回的内容（或内容中的一部分）
  */
 define('SECCODE_SMS_SUCCESS_FLAG', 'success');
 
@@ -52,6 +77,9 @@ define('SECCODE_SMS_MAX_ERR', 5);
  */
 define('SECCODE_SMS_PROVIDER_INFO', '<p>短信验证码由虎绿林提供</p>');
 
+
+///////////////////// 上传附件到百度云存储（BOS） /////////////////////
+
 /**
  * 百度 BCE 的 AK 和 SK
  */
@@ -73,6 +101,9 @@ define('BAIDUBCE_BOS_BUCKET', 'hu60');
  */
 define('BAIDUBCE_BOS_HOST', 'bj.bcebos.com');
 
+
+///////////////////// 上传附件到七牛云 /////////////////////
+
 /**
  * 七牛云 的 AK 和 SK
  */
@@ -93,3 +124,4 @@ define('QINIU_STORAGE_BUCKET', 'hu60');
  * 七牛云存储上传文件的HOST（用于服务器端签名）
  */
 define('QINIU_STORAGE_HOST', 'oeyz1bxso.bkt.clouddn.com');
+
