610af17a1429429171d0dfb3a0cd39218790b3d0
SwimmingTiger <hu60.cn@gmail.com>
2018-05-10 12:32:40 +0000

    缩略图：发送header

--- src/page/link/thumb.php
+++ src/page/link/thumb.php
@@ -1,6 +1,7 @@
 <?php
-// 作者: 幻阳化翼
+// 作者：幻阳化翼
 // 来自：https://hu60.net/q.php/bbs.topic.47473.html
+
 $img=$_GET['img'];
 $size=getimagesize($img);
 $src_w=$size[0];
@@ -16,18 +17,21 @@ if($size[2]==1)
 {
 	$src_image=imagecreatefromgif($img);
 	imagecopyresampled($new_image,$src_image,0,0,0,0,$w,$h,$src_w,$src_h);
+    header('Content-Type: image/gif');
 	imagegif($new_image,null);
 }
 elseif($size[2]==2)
 {
 	$src_image=imagecreatefromjpeg($img);
 	imagecopyresampled($new_image,$src_image,0,0,0,0,$w,$h,$src_w,$src_h);
+    header('Content-Type: image/jpeg');
 	imagejpeg($new_image,null,80);
 }
 else
 {
 	$src_image=imagecreatefrompng($img);
 	imagecopyresampled($new_image,$src_image,0,0,0,0,$w,$h,$src_w,$src_h);
+    header('Content-type: image/png'); 
 	imagepng($new_image,null);
 }
 imagedestroy($new_image);
