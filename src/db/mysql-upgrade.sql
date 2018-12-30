89b38730e2ea1ba9d073fc078da35ba95c41f6dc
幻阳化翼 <g726332269@gmail.com>
2018-12-30 17:07:09 +0800

    修复hu60_topic_favorites建表语句，添加id自增

--- src/db/mysql-upgrade.sql
+++ src/db/mysql-upgrade.sql
@@ -62,3 +62,6 @@ CREATE TABLE `hu60_topic_favorites` (
 ALTER TABLE `hu60_topic_favorites`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `unique_uid_and_topicId` (`uid`,`topic_id`);
+
+ALTER TABLE `hu60_topic_favorites`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
