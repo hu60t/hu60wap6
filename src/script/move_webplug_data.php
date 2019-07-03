8cd0c11bd1b03359936d22bbcc5bd25bd3cbd71c
SwimmingTiger <hu60.cn@gmail.com>
2019-07-04 01:00:04 +0800

    添加一个转移旧插件自定义数据到新表的脚本

---
+++ src/script/move_webplug_data.php
@@ -0,0 +1,24 @@
+<?php
+if ('cli' != php_sapi_name()) {
+	die('run in shell: php import_bk.php');
+}
+
+include '../config.inc.php';
+
+$db = (new db())->conn();
+$db->setAttribute(PDO::ERRMODE_EXCEPTION, true);
+
+$rs = $db->query("SELECT uid FROM `hu60_user` WHERE `info` like '%webplugData%'");
+foreach ($rs as $uid) {
+	$u = new user();
+	$u->uid($uid['uid']);
+	$u->virtualLogin();
+	echo "$u->uid $u->name:\n";
+	foreach ($u->getinfo('webplugData') as $k=>$v) {
+		echo "$k = ", str::cut($v, 0, 100, '...'), "\n";
+		$u->setData($k, $v);
+	}
+	$u->setinfo('webplugData', null);
+	echo "---------------------------------\n";
+}
+
