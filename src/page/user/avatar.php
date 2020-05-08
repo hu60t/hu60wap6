2bf78d63a923ce2ce11e3c013b15f96b1ad562bc
SwimmingTiger <hu60.cn@gmail.com>
2020-05-08 13:26:59 +0800

    头像上传支持审核后生效

--- src/page/user/avatar.php
+++ src/page/user/avatar.php
@@ -23,22 +23,27 @@ try {
 							"error"=>"文件大小超过限制！"
 				]));
 			}
+			
+			$review = $USER->hasPermission(UserInfo::PERMISSION_POST_NEED_REVIEW);
 
 			if (QINIU_STORAGE_AVATAR) {
-				$path = QINIU_AVATAR_PATH . $USER->uid.".jpg";
+				$path = QINIU_AVATAR_PATH . ($review ? 'review_' : '') . $USER->uid.".jpg";
 				$url = qiniu_upload($_FILES["avatar"]["tmp_name"], $path, true);
 				// 地址中加入一个随机数防止缓存问题
 				$url .= '?'.time();
 			} else {
-				$path = AVATAR_DIR . '/' . $USER->uid.".jpg";
+				$path = AVATAR_DIR . '/' . ($review ? 'review_' : '') . $USER->uid.".jpg";
 				move_uploaded_file($_FILES["avatar"]["tmp_name"], $path);
 				// 地址中加入一个随机数防止缓存问题
 				$url = $PAGE->getFileUrl($path).'?'.time();
 			}
 
+			if ($review) {
+				$url = str_replace('review_', '', $url);
+			}
 			$USER->setinfo("avatar.url", $url);
 			die(json_encode([
-						"message"=>"设置成功！"
+						"message"=>"设置成功！".($review ? '新头像在审核通过后生效。' : '')
 			]));
 		} catch (Exception $err) {
 			die(json_encode([                                                                                                                         "message" => '上传失败: '.$err->getMessage()
