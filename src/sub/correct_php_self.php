cc4eed0b05e9f1e85689cb613a6ec733029853f4
SwimmingTiger <hu60.cn@gmail.com>
2020-07-07 20:53:24 +0000

    对PATH_INFO进行urldecode，因为nginx不会进行urldecode，这会导致中文名称的聊天室无法访问

--- src/sub/correct_php_self.php
+++ src/sub/correct_php_self.php
@@ -9,4 +9,12 @@
  */
 
 //检查并重设置$_SERVER[PHP_SELF]，该变量在IIS6中FastCGI模式下运行的php中被误加PATH_INFO
-if ($_SERVER['PHP_SELF'] === $_SERVER['SCRIPT_NAME'] . $_SERVER['PATH_INFO']) $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'];
+if ($_SERVER['PHP_SELF'] === $_SERVER['SCRIPT_NAME'] . $_SERVER['PATH_INFO']) {
+    $_SERVER['PHP_SELF'] = $_SERVER['SCRIPT_NAME'];
+}
+
+// 对PATH_INFO进行urldecode，因为nginx不会进行urldecode，这会导致中文名称的聊天室无法访问
+if (isset($_SERVER['PATH_INFO'])) {
+    $_SERVER['PATH_INFO'] = urldecode($_SERVER['PATH_INFO']);
+}
+
