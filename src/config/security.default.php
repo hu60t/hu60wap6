abe472fc64c001e6406ae9f648db9d2f894d2395
SwimmingTiger <hu60.cn@gmail.com>
2020-07-27 10:48:00 +0800

    文件上传：更换域名

--- src/config/security.default.php
+++ src/config/security.default.php
@@ -123,7 +123,7 @@ define('QINIU_STORAGE_BUCKET', 'hu60');
 /**
  * 七牛云存储上传文件的HOST（用于服务器端签名）
  */
-define('QINIU_STORAGE_HOST', 'qiniu.img.hu60.cn');
+define('QINIU_STORAGE_HOST', 'file.hu60.cn');
 
 /**
  * 下载七牛资源时使用HTTPS
