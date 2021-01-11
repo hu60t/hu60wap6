d02d968fd8190188e685852d948f55c8e08de5e5
SwimmingTiger <hu60.cn@gmail.com>
2021-01-12 01:10:34 +0800

    现在可以通过 user.stat.json 接口获取会显示在html版页面底部的聊天室新消息了

--- src/page/user/stat.php
+++ src/page/user/stat.php
@@ -1,4 +1,42 @@
 <?php
-$tpl = $PAGE->start();
 $USER->start();
-$tpl->display('tpl:stat');
+JsonPage::start();
+
+$data = [
+    'uid' => $USER->uid,
+    'name' => $USER->name,
+    'isLogin' => $USER->islogin,
+    'newMsg' => 0,
+    'newAtInfo' => 0,
+    'newChats' => [],
+];
+
+// 新内信和新@消息条数
+if ($USER->islogin) {
+    $msg = msg::getInstance($USER);
+    $data['newMsg'] = $msg->newMsg();
+    $data['newAtInfo'] = $msg->newAtInfo();
+}
+
+// 聊天室新消息
+$chat = new chat;
+if (is_object($USER) && $USER->getinfo('chat.newchat_num') > 0) {
+    $newChatNum = $USER->getinfo('chat.newchat_num');
+} else {
+    $newChatNum = 1;
+}
+
+$newChats = $chat->newChats($newChatNum);
+
+$uinfo = new UserInfo;
+foreach ($newChats as &$v) {
+    $uinfo->uid($v['uid']);
+    $ubb = new UbbDisplay;
+    $uinfo->setUbbOpt($ubb);
+    JsonPage::selUbbP($ubb);
+    $v['content'] = $ubb->display($v['content'], true);
+}
+
+$data['newChats'] = $newChats;
+
+JsonPage::output($data);
