77d882dfd600e680aebd69f0abb250826cdab6d5
SwimmingTiger <hu60.cn@gmail.com>
2021-11-11 23:07:57 +0800

    支持通过POST传递所有参数：https://hu60.cn/q.php/bbs.topic.101950.html

--- src/sub/header_post_to_get.php
+++ src/sub/header_post_to_get.php
@@ -4,29 +4,35 @@ function header_to_get($header, $get) {
         $_GET[$get] = $_SERVER[$header];
     }
 }
-function post_to_get($name) {
-    if (!isset($_GET[$name]) && isset($_POST[$name])) {
-        $_GET[$name] = $_POST[$name];
-    }
-}
 
 header_to_get('HTTP_X_ORIGIN', '_origin');
-post_to_get('_origin');
-
 header_to_get('HTTP_X_UINFO', '_uinfo');
-post_to_get('_uinfo');
-
 header_to_get('HTTP_X_JSON', '_json');
-post_to_get('_json');
-
 header_to_get('HTTP_X_CONTENT', '_content');
-post_to_get('_content');
-
 header_to_get('HTTP_X_MYSELF', '_myself');
-post_to_get('_myself');
-
 header_to_get('HTTP_X_SID', '_sid');
-post_to_get('_sid');
-
 header_to_get('HTTP_X_TOPIC_SUMMARY', '_topic_summary');
-post_to_get('_topic_summary');
+header_to_get('HTTP_X_FLOOR_REVERSE', 'floorReverse');
+header_to_get('HTTP_X_PAGE_SIZE', 'pageSize');
+
+$_GET += $_POST;
+
+if (isset($_GET['_cid']) && isset($_GET['_pid']) && isset($_GET['_bid'])) {
+	$_arr = [$_GET['_cid'], $_GET['_pid']];
+	if (isset($_GET['_ext'])) {
+		if (is_array($_GET['_ext'])) {
+			$_arr = array_merge($_arr, $_GET['_ext']);
+		} else {
+			$_arr[] = $_GET['_ext'];
+		}
+	}
+	$_arr[] = $_GET['_bid'];
+	$_SERVER['PATH_INFO'] = '/'.implode('.', $_arr);
+	$_SERVER['DOCUMENT_URI'] = $_SERVER['SCRIPT_NAME'].$_SERVER['PATH_INFO'];
+	$_SERVER['REQUEST_URI'] = $_SERVER['DOCUMENT_URI'];
+	if (!empty($_SERVER['QUERY_STRING'])) {
+		$_SERVER['REQUEST_URI'] .= '?'.$_SERVER['QUERY_STRING'];
+	}
+	unset($_arr);
+}
+
