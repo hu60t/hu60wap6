e0444bd2de3080c1151f4e27af968cc96d9d72ab
SwimmingTiger <hu60.cn@gmail.com>
2022-02-05 22:28:21 +0800

    删除不再需要的src/css.php

--- src/css.php
+++
@@ -1,9 +0,0 @@
-<form action="" method="post">
-    <textarea name="data" cols="120" rows="25"></textarea><br/>
-    <input type="submit" value="CSS 转移"/>
-</form>
-<?
-$content = $_POST['data'];
-$content = str_replace("{", " { ", $content);
-$content = str_replace("}", " }<br/>", $content);
-echo $content;
