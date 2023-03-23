ee09e72ef93cb53012181846418d526f3c2aa4e2
SwimmingTiger <hu60.cn@gmail.com>
2023-03-24 00:08:49 +0800

    api.msg.isread.json接口支持设置全部消息为已读，支持获取未读消息id列表

--- src/page/api/msg.php
+++ src/page/api/msg.php
@@ -16,10 +16,10 @@ $action = str::word($PAGE->ext[1]);
 if (!in_array($action, ['get', 'set'])) {
     throw new Exception("未知操作 $action");
 }
-$isSet = ($action == 'get');
+$isSet = ($action == 'set');
 
-if (isset($_GET['data'])) {
-    $data = $_GET['data'];
+if (isset($_POST['data'])) {
+    $data = $_POST['data'];
 } else {
     $data = file_get_contents('php://input');
 }
@@ -28,24 +28,49 @@ $data = json_decode($data, true);
 if (!is_array($data)) {
     throw new Exception('请求体应该是一个JSON对象');
 }
-if (!is_array($data['ids'])) {
+if (isset($data['ids']) && !is_array($data['ids'])) {
     throw new Exception('ids字段必须是数组');
 }
+if (isset($data['type']) && !is_int($data['type'])) {
+    throw new Exception('type字段必须是整数：0为内信，1为@消息');
+}
+if (isset($data['ids']) && isset($data['type'])) {
+    throw new Exception('ids和type字段不能同时存在');
+}
 
-$msg = new msg;
+$msg = new msg($USER);
 
 $result = [];
-foreach ($data['ids'] as $id) {
-    $rs = $msg->get_msg($USER->uid, $id, 'isread');
-    if (is_array($rs)) {
-        $isread = $rs['isread'];
-        if ($action == 'set' && !$isread) {
-            $result[$id] = $msg->update_msg($USER->uid, $id);
-        } else {
-            $result[$id] = (bool)$isread;
-        }
+
+if (isset($data['type'])) {
+    if ($isSet) {
+        $rs = $msg->readAll($data['type']);
+        $result['update'] = $rs ? $rs->rowCount() : false;
     } else {
-        $result[$id] = null;
+        $offset = max((int)$data['offset'], 0);
+        $size = min((int)$data['size'], 1000);
+        if ($size < 1) {
+            $size = 1000;
+        }
+
+        $rs = $msg->msgList($data['type'], $offset, $size, 0, 'id');
+        foreach ($rs as $item) {
+            $result[$item['id']] = false;
+        }
+    }
+} else {
+    foreach ($data['ids'] as $id) {
+        $rs = $msg->get_msg($USER->uid, $id, 'isread');
+        if (is_array($rs)) {
+            $isread = $rs['isread'];
+            if ($isSet && !$isread) {
+                $result[$id] = $msg->update_msg($USER->uid, $id);
+            } else {
+                $result[$id] = (bool)$isread;
+            }
+        } else {
+            $result[$id] = null;
+        }
     }
 }
 
