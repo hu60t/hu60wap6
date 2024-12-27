5b7474fe21e59f9e6db4368eb8f9cf1ce5648e63
SwimmingTiger <hu60.cn@gmail.com>
2024-12-27 20:29:54 +0800

    lutris: 为鉴权页面添加超速限制

--- src/page/lutris/acl.php
+++ src/page/lutris/acl.php
@@ -3,6 +3,21 @@
 * 通过JS跳转确保下载者使用的是真实浏览器
 */
 
+/**
+ * CC判定范围
+ */
+$CC_LIMIT = [
+    10,  // n秒内
+    5, // 最多访问n次
+];
+
+// 前面加个空格，改变 IP hash，让 file.winegame.net 的 超速记录和 hu60.cn 的不在一起，
+// 防止从 hu60.cn 过来的用户一点下载链接就超速（因为 hu60.cn 的上限更高）
+$CC_REAL_IP = " ".$CC_REAL_IP;
+
+// 加载防CC模块
+require_once SUB_DIR.'/cc.php';
+
 $url = $_GET['u'];
 $time = time();
 $time -= $time % 30;
